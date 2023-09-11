d70c2e5b98348690b00c05d6cbf2f7e3ad9c032e
Jonas_Jones <me@jonasjones.dev>
2023-09-11 23:17:09 +0200

    Update likedsongsync2.py
    fixed ETA calculation

--- likedsongsync2.py
+++ likedsongsync2.py
@@ -150,6 +150,6 @@ if __name__ == "__main__":
         if not is_track_in_playlist(liked_songs_playlist_songs, track_uri):
             add_track_to_playlist(LIKEDSONGPLAYLIST_ID, track_uri)
             if VERBOSE_LOGGING:
-                verboseprint("%-10s %15s" % (f"ETA:{round((((int(len(liked_songs))-tracknr)*0.25)/60))}min", f"[{tracknr}/{int(len(liked_songs))}|+]") + "%30.32s %s" % (track['artists'][0]['name'], track['name']))
+                verboseprint("%-10s %15s" % (f"ETA:{round((((int(len(liked_songs))-tracknr)*0.75)/60))}min", f"[{tracknr}/{int(len(liked_songs))}|+]") + "%30.32s %s" % (track['artists'][0]['name'], track['name']))
         else:
-            verboseprint("%-10s %13s" % (f"ETA:{round((((int(len(liked_songs))-tracknr)*0.25)/60))}min", f"[{tracknr}/{int(len(liked_songs))}]") + "%32.32s %s" % (track['artists'][0]['name'], track['name']))
+            verboseprint("%-10s %13s" % (f"ETA:{round((((int(len(liked_songs))-tracknr)*0.`75)/60))}min", f"[{tracknr}/{int(len(liked_songs))}]") + "%32.32s %s" % (track['artists'][0]['name'], track['name']))
