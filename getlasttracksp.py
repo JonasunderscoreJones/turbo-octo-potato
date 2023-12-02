42a6e153fa1bd8133b0d67c42513314ec69d7f62
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2023-12-02 19:12:42 +0100

    Refactor to obey to pep-8

--- getlasttracksp.py
+++ getlasttracksp.py
@@ -1,6 +1,8 @@
-import spotipy, os
-from spotipy.oauth2 import SpotifyOAuth
+'''Get the last spotify track'''
+import os
 from dotenv import load_dotenv
+
+import top_lib
 
 # load .env file
 load_dotenv()
@@ -11,24 +13,24 @@ SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 SPOTIFY_REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
 PLAYLIST_ID = os.getenv('SOMEPLAYLIST_ID')
 
-if not SPOTIFY_CLIENT_ID or not SPOTIFY_CLIENT_SECRET or not SPOTIFY_REDIRECT_URI or not PLAYLIST_ID:
-		raise ValueError("Please provide the required information in the .env file.")
+if not SPOTIFY_CLIENT_ID or not SPOTIFY_CLIENT_SECRET or \
+    not SPOTIFY_REDIRECT_URI or not PLAYLIST_ID:
+    raise ValueError("Please provide the required information in the .env file.")
 
 # Create a Spotipy instance with authentication
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET, redirect_uri=SPOTIFY_REDIRECT_URI, scope="playlist-read-private"))
+auth_manager = top_lib.Auth(verbose=True)
+sp = auth_manager.newSpotifyauth("playlist-read-private")
 
-def get_last_track_of_playlist(playlist_id):
+def get_last_track_of_playlist(playlist_id: str) -> dict:
+    '''Get the last track of a playlist'''
     # Get the playlist's tracks
     playlist = sp.playlist_tracks(playlist_id)
 
     # Extract the last track
-    last_track = playlist["items"][-1]["track"]
+    return playlist["items"][-1]["track"]
 
-    return last_track
-
-# Replace "YOUR_PLAYLIST_ID" with the actual playlist ID
-playlist_id = PLAYLIST_ID
-last_track = get_last_track_of_playlist(playlist_id)
+# Retrieve the last track of the playlist
+last_track = get_last_track_of_playlist(PLAYLIST_ID)
 
 # Access information about the last track
 print("Last Track Name:", last_track["name"])
