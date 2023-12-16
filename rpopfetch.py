0b5edb5b3a1d81649f7938e8892d7e65be9d4760
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2023-12-16 03:46:51 +0100

    Fixed previous fix

--- rpopfetch.py
+++ rpopfetch.py
@@ -182,7 +182,7 @@ def convert_monthly_content_to_json(content, year, month):
             parts[0] = parts[0].replace('th', '').replace('st', '').replace('nd', '').replace('rd', '')
 
             # make the links an empty list if it's null
-            if parts[5] == None:
+            if parts[5] == [None]:
                 parts[5] = []
             
             # create a json entry from the parsed data
