4dbe771bdc794d5eeae9618fc35deeaa173bf861
J-onasJones <me@jonasjones.dev>
2024-10-08 20:26:36 +0200

    moved pixel coordinates to global constants

--- tabletop_achievements/tabletop_savetochest_loop.py
+++ tabletop_achievements/tabletop_savetochest_loop.py
@@ -2,20 +2,30 @@ import pyautogui
 import time
 import os
 
+RIGHTCLICK_X = 1870
+RIGHTCLICK_Y = 675
+SAVECONTEXTMENUCLICK_X = 2030
+SAVECONTEXTMENUCLICK_Y = 875
+NAMEFIELDCLICK_X = 1700
+NAMEFIELDCLICK_Y = 1110
+SAVECLICK_X = 1770
+SAVECLICK_Y = 1200
+
+
 time.sleep(3)
 
 while True:
     # Click at pixel (1870, 675)
-    pyautogui.moveTo(1870, 675)
-    pyautogui.click(1870, 675, button='right')
+    pyautogui.moveTo(RIGHTCLICK_X, RIGHTCLICK_Y)
+    pyautogui.click(RIGHTCLICK_X, RIGHTCLICK_Y, button='right')
     time.sleep(0.15)
 
     # Click at pixel (2030, 875)
-    pyautogui.click(2030, 875)
+    pyautogui.click(SAVECONTEXTMENUCLICK_X, SAVECONTEXTMENUCLICK_Y)
     time.sleep(0.15)
 
     # Click at pixel (1700, 1110)
-    pyautogui.click(1700, 1110)
+    pyautogui.click(NAMEFIELDCLICK_X, NAMEFIELDCLICK_Y)
     time.sleep(0.15)
 
     # Get the current Unix timestamp
@@ -25,5 +35,5 @@ while True:
     pyautogui.write(timestamp)
 
     # Click at pixel (1770, 1200)
-    pyautogui.click(1770, 1200)
+    pyautogui.click(SAVECLICK_X, SAVECLICK_Y)
     time.sleep(0.2)
